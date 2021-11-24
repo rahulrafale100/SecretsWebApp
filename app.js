6c6c24936bd21483f7f6ee3551fa09d33dcbe9b8
Rahul Kumar Maurya <rahulrafale100@gmail.com>
2021-11-24 12:10:07 +0530

    Hashing Security Added

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser=require("body-parser");
 const ejs=require("ejs");
 const mongoose=require("mongoose");
 const encrypt=require("mongoose-encryption");
+const md5=require("md5");
 
 const app=express();
 
@@ -15,7 +16,7 @@ const userSchema=new mongoose.Schema({
     username:String,
     password:String
 });
-userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
+//  userSchema.plugin(encrypt,{secret:process.env.SECRET,encryptedFields:["password"]});
 
 const User=mongoose.model("User",userSchema);
 
@@ -31,7 +32,7 @@ app.get("/register",function(req,res){
 app.post("/register",function(req,res){
     const newUser=new User({
         username:req.body.username,
-        password:req.body.password
+        password:md5(req.body.password)
     });
     newUser.save(function(err){
         if(!err){
@@ -44,7 +45,7 @@ app.post("/register",function(req,res){
 });
 app.post("/login",function(req,res){
     const uname=req.body.username;
-    const pswd=req.body.password;
+    const pswd=md5(req.body.password);
     User.findOne({username:uname},function(err,found){
         if(!err){
             if(found.password===pswd){
